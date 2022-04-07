00d7ba5c09953bb84dcd2edc8059afe4756ccd26
devops-vineet <vineet.aryan@gmail.com>
2022-04-07 15:39:07 +0530

    first commit

---
+++ Alexa-4-Ch-Switch/arduino_secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "DIGISOL"  //Enter WIFI NAME
+#define SECRET_PASS "vineet@321"  //Enter WIFI PASSWORD
+#define SECRET_SSID "realme 7i"  //Enter WIFI NAME
+#define SECRET_PASS "vineet@321"  //Enter WIFI PASSWORD
+#define SECRET_DEVICE_KEY "OOQK8PGCNBZCUUDHFGQN"  //Enter Secret Key
